6f83de40ab50e8915661cf16277e407332fde0fa
chentsafir <chenxdtzz@gmail.com>
2025-06-17 11:03:23 +0300

    add .env in client

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // const API_URL = import.meta.env.VITE_APP_BASE_URL + "/api";
-const API_URL = "http://localhost:3001/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const baseQuery = fetchBaseQuery({ baseUrl: API_URL });
 
